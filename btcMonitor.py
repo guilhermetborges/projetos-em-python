91f6bf655c33f1225f6683687844aebb6424b3c7
guilhermetborges <112335378+guilhermetborges@users.noreply.github.com>
2025-01-29 18:27:17 -0300

    Update btcMonitor.py
    trocando para mudar os valores em tempo real

--- btcMonitor.py
+++ btcMonitor.py
@@ -20,42 +20,51 @@ janela.geometry("350x350")
 janela.config(bg=corcinza)
 
 ttk.Separator(janela,orient=HORIZONTAL).grid(row=0,columnspan=1,ipadx=200)
+def info():
+    #importando apis
 
-#importando apis
+    api_link = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD%2CEUR%2CCNY%2CBRL'
 
-api_link = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD%2CEUR%2CCNY%2CBRL'
+    #api request
+    response = requests.get(api_link)
 
-#api request
-response = requests.get(api_link)
+    #convertendo api
 
-#convertendo api
+    dados = response.json()
 
-dados = response.json()
+    print(dados)
 
-print(dados)
+    valor_usd = float(dados['USD'])
+    valor_format_usd = "${:,.3f}".format(valor_usd)
+    l_usd['text'] = ' USD: '+valor_format_usd
 
-valor_usd = float(dados['USD'])
-valor_format_usd = "${:,.3f}".format(valor_usd)
 
-valor_real = float(dados['BRL'])
-valor_format_real = "R${:,.3f}".format(valor_real)
+    valor_real = float(dados['BRL'])
+    valor_format_real = "R${:,.3f}".format(valor_real)
+    l_real['text'] = ' REAL: '+valor_format_real
 
-valor_euro = float(dados['EUR'])
-valor_format_euro = "€{:,.3f}".format(valor_euro)
+    valor_euro = float(dados['EUR'])
+    valor_format_euro = "€{:,.3f}".format(valor_euro)
+    l_euro['text'] = ' EURO: '+valor_format_euro
 
-valor_yuan = float(dados['CNY'])
-valor_format_yuan = "¥{:,.3f}".format(valor_yuan)
+    valor_yuan = float(dados['CNY'])
+    valor_format_yuan = "¥{:,.3f}".format(valor_yuan)
+    l_kaz['text'] = ' YUAN: '+valor_format_yuan
+
+    frame_baixo.after(1000,info)
+
+
 
 
 
 
 #criando frames
 
-frame_cima = Frame(janela, width=350,height=60,pady=0,padx=0,bg=corbranca)
+frame_cima = Frame(janela, width=410,height=60,pady=0,padx=0,bg=corbranca)
 frame_cima.grid(row=0,column=0)
 
 
-frame_baixo = Frame(janela, width=350,height=290,pady=0,padx=0,bg=corpreta)
+frame_baixo = Frame(janela, width=410,height=290,pady=0,padx=0,bg=co0)
 frame_baixo.grid(row=1,column=0)
 
 
@@ -69,18 +78,18 @@ l_crypto.place(x=33,y=12)
 
 #frame baixo
 
-l_usd = Label(frame_baixo,text=valor_format_usd,width=14,padx=5,relief='flat',anchor='center',font="Arial 19",bg=corpreta,fg='red')
-l_usd.place(x=50,y=20)
+l_usd = Label(frame_baixo,text='',width=25,padx=3,relief='flat',anchor='nw',font="Arial 18",bg=co0,fg=co1)
+l_usd.place(x=20,y=20)
 
-l_euro = Label(frame_baixo,text=valor_format_euro,width=14,padx=5,relief='flat',anchor='center',font="Ivy 15 bold",bg=corpreta,fg='red')
-l_euro.place(x=15,y=90)
+l_euro = Label(frame_baixo,text='',width=25,padx=3,relief='flat',anchor='nw',font="Ivy 12 bold",bg=co0,fg=co1)
+l_euro.place(x=5,y=90)
 
-l_real = Label(frame_baixo,text=valor_format_real,width=14,padx=5,relief='flat',anchor='center',font="Ivy 15 bold",bg=corpreta,fg='red')
-l_real.place(x=17,y=160)
+l_real = Label(frame_baixo,text='',width=25,padx=3,relief='flat',anchor='nw',font="Ivy 12 bold",bg=co0,fg=co1)
+l_real.place(x=6,y=160)
 
-l_kaz = Label(frame_baixo,text=valor_format_yuan,width=14,padx=5,relief='flat',anchor='center',font="Ivy 15 bold",bg=corpreta,fg='red')
-l_kaz.place(x=15,y=230)
+l_kaz = Label(frame_baixo,text='',width=25,padx=3,relief='flat',anchor='nw',font="Ivy 12 bold",bg=co0,fg=co1)
+l_kaz.place(x=5,y=230)
 
 
-
+info()
 janela.mainloop()
